176f72216c93235bc7dbdb5e94f7675c60b4fc5a
kaur-randeep <randeepbal3321@gmail.com>
2024-07-24 22:04:54 -0400

    Adding Bash Scripts

---
+++ Week 12/myScript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# A simple script that uses variables
+
+echo " I am logged into the $UESR account."
+echo "The filename for this script is $0."
+echo "The input for this file is $1."
+exit 
